9d3b5f3a5284c9a48753cc71f2e8cb9966d78786
Max Khairulov <max.hairulov@mail.ru>
2023-06-04 13:50:15 +0400

    Fix in lab5

--- src/lab2/Main2.java
+++ src/lab2/Main2.java
@@ -6,9 +6,15 @@ import lab1.*;
 public class Main2 {
 
     public static void main(String[] args) throws IOException {
-        Car car = new Car(200, "Toyota", 5000, 4);
+        Car car = new Car(2200, "Toyota", 5000, 4);
         FileOutputStream fos = new FileOutputStream("./res/output.txt");
         IOUtils.serialize(car, fos);
         fos.close();
+
+
+        FileInputStream fis = new FileInputStream("./res/output.txt");
+        Transport car1 = IOUtils.deserialize(fis);
+        System.out.println(car1);
+        fis.close();
     }
 }
